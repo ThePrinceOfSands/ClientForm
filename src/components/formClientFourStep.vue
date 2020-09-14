82ee6d0841caf2973f2445b80a034d385fb749f4
Yuriy <example@gmail.com>
2020-09-14 10:58:33 +0500

    Chenge path

--- src/components/formClientFourStep.vue
+++ src/components/formClientFourStep.vue
@@ -7,26 +7,13 @@
 
     <div class="confirmation__checkMark"></div>
 
-    <div class="buttonAndWarning">
-      <div class="buttons">
-        <button class="buttons__stepBack buttons__stepBack__clientFour"
-                @click.prevent="nextStepBack"
-        > Назад</button>
 
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "formClientFourStep",
-  props: ['backStep'],
-  methods: {
-    nextStepBack() {
-      this.backStep({});
-    }
-  }
+  name: "formClientFourStep"
 }
 </script>
 
